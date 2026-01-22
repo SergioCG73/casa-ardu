83fe8f573114c1a3831136ec02bfe1d45bcf88b5
Sergio Cano - Sótano <sergioc_g@hotmail.es>
2026-01-22 09:25:53 +0100

    Limpieza de código

--- inicio.php
+++ inicio.php
@@ -14,51 +14,13 @@
         <button id="btnHB10" name="btnHB10">HB 10</button>
         <button id="btnSulfacid" name="btnSulfacid">SulfaCID</button>
     </div>
-
-    <!--<div id="mezcladores">
-        <fieldset><legend>Mezcladores</legend>
-            <input type="radio" id="m214" name="mezclador" value="M214">
-            <label for="m214">Mezclador M214</label><br>
-            <input type="radio" id="m215" name="mezclador" value="M215">
-            <label for="m215" >Mezclador M215</label><br>
-        </fieldset>        
-    </div>  
-    
-    <div id="recetas">
-        <fieldset><legend>Recetas</legend>
-            <input type="radio" id="receta_1" name="recetas" value="Receta 1">
-            <label for="receta_1">Receta 1</label><br>
-            <input type="radio" id="receta_2" name="recetas" value="Receta 2">
-            <label for="receta_2" >Receta 2</label><br> 
-            <input type="radio" id="receta_3" name="recetas" value="Receta 3">
-            <label for="receta_3" >Receta 3</label><br>     
-            <input type="radio" id="receta_4" name="recetas" value="Receta 4">
-            <label for="receta_4" >Receta 4</label><br>
-        </fieldset>
-    </div>
-
-    <div id="reactores">
-        <fieldset><legend>Reactores</legend>
-        <input type="radio" id="R200" name="reactores" value="R200">
-        <label for="R200">R200</label>
-        <input type="radio" id="R201" name="reactores" value="R201">
-        <label for="R201">R201</label>
-        <input type="radio" id="R202" name="reactores" value="R202">
-        <label for="R202">R202</label>
-    </div>
-
-    <div id="pesos">
-        <label for="peso_inicial">Peso Inicial: </label>
-        <input type="text" id="peso_inicial" name="peso_inicial">
-    </div>
-
-    <p id="numeroproduccion"></p>
-
-    <div name="botonera">
-        <button id="btnValidar" name="btnValidar">Validar</button>
-        <p id="resultado"></p>   
-        <input type="hidden" id="fechaHora" name="fechaHora">     
-    </div>-->
+<!--
+    <div id="miModal" class="modal">
+        <div class="modal-contenido">
+            <p id="mensajeModal"></p>
+            <button id="btnAceptar">Aceptar</button>
+        </div>
+    </div>    -->
 </body>
     <script src="js/inicio.js"></script>
 </html>
