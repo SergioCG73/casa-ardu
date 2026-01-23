ebaaf1f7767a722cf9f7bebb5f143150c57e7460
Sergio Cano - Sótano <sergioc_g@hotmail.es>
2026-01-23 10:27:28 +0100

    Se añade UPDATE tabla equipos

--- enviardatos.php
+++ enviardatos.php
@@ -2,51 +2,45 @@
 
 //Este fichero recibe los datos enviados por AJAX desde index.js y los inserta en la tabla fab_en_curso
 
+    ob_clean();
+    header('Content-Type: application/json; charset=utf-8');
+    error_reporting(0);
+    ini_set('display_errors', 0);
+
 // Envio de datos a la tabla fabricaciones_en_curso
     //Verificar si los datos se recibieron desde AJAX    
         
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {        
     // Recoger los datos enviados por AJAX
 
-        require_once("miconexion.php");
-        $fechaHoraInicio = $_POST["fechaHoraInicio"] ?? "";        
-        $mezclador = $_POST["mezclador"] ?? "";
-        $reactor = $_POST["reactor"] ?? "";
-        $receta = $_POST["receta"] ?? "";
-        $numeroProduccion = $_POST["numeroProduccion"] ?? "";
-        $producto = $_POST["producto"] ?? "";
-
-        /*$receta = $_POST["receta"] ?? 0;
-        $numFabricacion = $_POST['numFabricacion'] ?? '';
-        $producto = $_POST['producto'] ?? '';*/
-       
-       /* $respuesta = [
-            "ok" => true,
-            "datos" => $_POST
-        ];
-
-         header('Content-Type: application/json');
-         echo json_encode($respuesta);         */
+    require_once("miconexion.php");
+    $fechaHoraInicio = $_POST["fechaHoraInicio"] ?? "";        
+    $mezclador = $_POST["mezclador"] ?? "";
+    $reactor = $_POST["reactor"] ?? "";
+    $receta = $_POST["receta"] ?? "";
+    $numeroProduccion = $_POST["numeroProduccion"] ?? "";
+    $producto = $_POST["producto"] ?? "";        
 
     //Validación de datos recibidos
 
     if (
-    empty($fechaHoraInicio) ||
-    empty($mezclador) ||
-    empty($reactor) ||
-    empty($receta) ||
-    empty($numeroProduccion) ||
-    empty($producto)
+        empty($fechaHoraInicio) ||
+        empty($mezclador) ||
+        empty($reactor) ||
+        empty($receta) ||
+        empty($numeroProduccion) ||
+        empty($producto)
     ) {
-    header('Content-Type: application/json');
-    echo json_encode([
-        'ok' => false,
-        'message' => 'Faltan datos'
+        header('Content-Type: application/json');
+        echo json_encode([
+            'ok' => false,
+            'message' => 'Faltan datos'
     ]);
-    exit;
-    }
+        exit;
+    }    
 
     try {
+    //SQL para INSERT datos en fabricaciones_en_curso
         $insertSQL = "INSERT INTO fabricaciones_en_curso
                       (FechaInicio, Mezclador, Reactor, Receta, NumeroFabricacion, Producto_id)
                       VALUES (:fechaHoraInicio, :Mezclador, :Reactor, :Receta, :numeroProduccion, :Producto)";
@@ -59,21 +53,29 @@
         $insertStmt->bindParam(":numeroProduccion", $numeroProduccion, PDO::PARAM_STR);
         $insertStmt->bindParam(":Producto", $producto, PDO::PARAM_STR);
 
-        $insertStmt->execute();
+        $insertStmt->execute();    
 
-        header('Content-Type: application/json');
+    //SQL para UPDATE estados en la tabla equipos
+    
+        $updateSQL ="UPDATE equipos
+                     SET Estado = 'En uso'
+                     WHERE Equipo_id = :Mezclador";
+
+        $updateStmt = $conexion->prepare($updateSQL);
+        $updateStmt-> bindParam(":Mezclador", $mezclador, PDO::PARAM_STR);
+
+        $updateStmt->execute();
+        
         echo json_encode([
-            'ok' => true,
-            'message' => 'Datos guardados correctamente'
+            "ok" => true,
+            "message" => "Datos guardados correctamente"
         ]);
         exit;
 
-    } catch (PDOException $e) {
-
-        header('Content-Type: application/json');
+    } catch (PDOException $e) {        
         echo json_encode([
-            'ok' => false,
-            'error' => $e->getMessage()
+            "ok"=> false,
+            "error" => $e->getMessage()
         ]);
         exit;
     }
