0ba9487302728b2f8dc73e273ff6e82b301f3515
Sergio Cano - Sótano <sergioc_g@hotmail.es>
2026-02-09 09:21:05 +0100

    Se agregan comentarios

--- public/index.php
+++ public/index.php
@@ -17,12 +17,12 @@ if (!class_exists($controllerClass)) {
     die("La clase $controllerClass no existe");
 }
 
-$controller = new $controllerClass();
+$controller = new $controllerClass(); //Crear el objeto $controller con la clase que toca
 
 if (!method_exists($controller, $a)) {
     die("El método $a no existe en $controllerClass");
 }
 
-$controller->$a();
+$controller->$a(); //Al entrar en la app llama a index()
 
 
