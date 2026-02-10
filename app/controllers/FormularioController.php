2047a8e3387bcaba1ff482531608eaa0f09380e5
Sergio Cano - Sótano <sergioc_g@hotmail.es>
2026-02-10 10:20:22 +0100

    Se agregan métodos para los formularios de Sulfato y Férrico

--- app/controllers/FormularioController.php
+++ app/controllers/FormularioController.php
@@ -2,6 +2,19 @@
 
 class FormularioController {
     public function mostrar() {
-        require_once '../app/views/formulario.php';
+        //require_once "../app/views/formulario.php"; //funciona sin DIR
+        //require_once __DIR__ . "/../views/formulario.php";
+        require_once __DIR__ . "/../views/formP18.php";
     }
+
+    public function sulfato() {
+        //require_once "../app/views/formSulfato.php"; funciona sin DIR
+        require_once __DIR__ . "/../views/formSulfato.php";
+    }
+
+    public function ferrico() {
+        //require_once "../app/views/formSulfato.php"; funciona sin DIR
+        require_once __DIR__ . "/../views/formFerrico.php";
+    }
+
 }
