4b8a267e1ba032b204f0b6b25f19a4a73b6e2f53
Sergio Cano - Sótano <sergioc_g@hotmail.es>
2026-02-10 10:21:17 +0100

    Se crea nuevo model leerdatos

---
+++ app/models/leerdatos.php
@@ -0,0 +1,63 @@
+<?php
+
+ob_clean();
+header('Content-Type: application/json; charset=utf-8');
+error_reporting(0);
+ini_set('display_errors', 0);
+
+require_once("miconexion.php");
+
+// Entrada combinada: JSON o POST
+$input = json_decode(file_get_contents("php://input"), true);
+$modo = $input["modo"] ?? $_POST["modo"] ?? null;
+$producto = $input["producto"] ?? $_POST["producto"] ?? null;
+
+    if ($producto === "P18") {
+        $tabla = "p18_terminadas";
+    } elseif ($producto === "Sulfato") {
+        $tabla = "sulfato_terminadas";
+        $producto = "Sulfato";
+    } elseif ($producto === "Ferrico") {
+        $tabla = "ferrico_terminadas";        
+        $producto = "ferrico";        
+    } 
+    else {
+        return ["ok" => false, "error" => "Producto no válido"];
+    }
+
+    // 1) Última producción terminada
+    $sqlSelect = "SELECT NumeroFabricacion 
+            FROM $tabla
+            ORDER BY NumeroFabricacion DESC 
+            LIMIT 1";
+    $stmt = $conexion->prepare($sqlSelect);
+    $stmt->execute();
+    $ultimaAcabada = $stmt->fetchColumn() ?? 0;
+
+    // 2) Última producción en curso
+    $sqlSelect = "SELECT NumeroFabricacion
+                  FROM  fabricaciones_en_curso
+                  WHERE producto_id = :producto
+                  ORDER BY NumeroFabricacion DESC
+                  LIMIT 1
+                  ";    
+    
+    $stmt = $conexion->prepare($sqlSelect);
+    $stmt->bindParam(":producto", $producto, PDO::PARAM_STR);
+    $stmt->execute();
+    $ultimaEnCurso = $stmt->fetchColumn();
+
+    $siguienteFabricacion = max($ultimaAcabada, $ultimaEnCurso) + 1;
+
+
+    echo json_encode([
+        "ok" => true,
+        "modo" => $modo,
+        "producto" => $producto,
+        "ultimaAcabada" => $ultimaAcabada,
+        "ultimaEnCurso" => $ultimaEnCurso,
+        "siguienteFabricacion" => $siguienteFabricacion
+]); 
+
+     
+exit;
