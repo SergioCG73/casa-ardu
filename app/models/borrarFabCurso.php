2be8535dc5a468c60fc2ace1c73ec0756c6b41ef
Sergio Cano - Sótano <sergioc_g@hotmail.es>
2026-02-19 17:45:23 +0100

    Se agrega los update de mezclador y reactor cuando se borra la producción

--- app/models/borrarFabCurso.php
+++ app/models/borrarFabCurso.php
@@ -6,24 +6,43 @@ error_reporting(0);
 ini_set('display_errors', 0);
 
 require_once("miconexion.php");
- 
 
-// Recibir JSON
 $input = json_decode(file_get_contents("php://input"), true);
+
+$mezclador = $input["Mezclador"];
+$reactor = $input["Reactor"];
 
 $NumeroFabricacion = $input["NumeroFabricacion"];
 
-// Consulta segura
 $sqlDelete = "DELETE FROM fabricaciones_en_curso
               WHERE NumeroFabricacion = :num";
 
 $stmt = $conexion->prepare($sqlDelete);
 $stmt->bindParam(":num", $NumeroFabricacion);
-
-// Ejecutar
 $ok = $stmt->execute();
 
-// Respuesta
+$sqlUpdate = "UPDATE equipos SET Estado = 'Vacio' WHERE Equipo_id = :mezclador";
+$stmt = $conexion->prepare($sqlUpdate);
+$stmt->bindParam(":mezclador", $mezclador);
+$rtdo = $stmt->execute();
+
+
+$sqlUpdate = "UPDATE equipos SET Estado = 'Vacio' WHERE Equipo_id = :reactor";
+$stmt = $conexion->prepare($sqlUpdate);
+$stmt->bindParam(":reactor", $reactor);
+$rtdo = $stmt->execute();
+
+/*echo json_encode([
+    "ok" => true,
+    "input" => $input,
+    "mezclador" => $mezclador,
+    "reactor" => $reactor,
+    "mensaje" => "Update OK"
+]); exit; */
+
+
+
+
 echo json_encode([
     "ok" => $ok,
     "status" => $ok ? "Registro eliminado" : "Error al eliminar",
