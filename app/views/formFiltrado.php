f20eb19871567a96cf92ad096251e3ea50bf1357
Sergio Cano - Sótano <sergioc_g@hotmail.es>
2026-02-20 13:10:54 +0100

    Modificaciones varias

--- app/views/formFiltrado.php
+++ app/views/formFiltrado.php
@@ -1,64 +1,67 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/HTML/public/css/style.css" rel="stylesheet" type="text/css">
+    <link href="/HTML/public/css/styleFiltrado.css" rel="stylesheet" type="text/css">
     <title>FiltradoP18</title>    
 </head>
 <body>
     <div id="contenedor" class="contenedor">
         <h1 id="titulo-formulario">Filtrado Policloruro</h1>
-
         <div id="formulario">
-            <div id="fabricaciones"><h1></h1>  </div>
-
-            <div id="reactores">
-                <fieldset>
-                    <legend>Reactores</legend>
-                    <div class="radio-group">
-                        <label class="radio-label"><input type="radio" name="sel_reactor" value="R1"> Reactor 1</label>
-                        <label class="radio-label"><input type="radio" name="sel_reactor" value="R2"> Reactor 2</label>
-                    </div>
-
-                    <div class ="input-group-Reactores">
-                        <label for="peso_inicial_reactor" class="input-label">Peso inicial reactor (Kg):</label>
-                        <input type="text" id="peso_inicial_reactor" name="peso_inicial_reactor"> 
-                    
-                        <label for="peso_final_reactor" class="input-label">Peso final reactor (Kg):</label>
-                        <input type="text" id="peso_final_reactor" name="peso_final_reactor"> 
-                    </div>
-                </fieldset>
-            </div>
-
-            <div id="recetas">
-                <fieldset>
-                    <legend>Recetas</legend>
-                    <div class="radio-group">
-                        <label class="radio-label"><input type="radio" name="sel_receta" value="P18"> P18</label>
-                        <label class="radio-label"><input type="radio" name="sel_receta" value="Papilla"> Papilla</label>
-                    </div>
-                </fieldset>
-            </div>
-
-            <div id="botonera" class="botonera">
-                <button id="btnRetroceder" class="boton">Retroceder</button>                
-                <button id="btnCrear" class="boton">Crear</button>                
-            </div>
-        </div>
-
-        <div id="resultados" class="resultados">
-            <p id="displayProduccion" name="displayProduccion"></p>
-        </div>
+            <div id="fabricaciones">
+                <h2></h2>
+            </div>                   
         
-        <div id="modal" class="mi-modal">
+        <div id="analitica">
+            <fieldset><legend>Analítica</legend>
+                <label>Densidad</label>
+                <input id="densidad" class="densidad">
+                <br><br><br>
+                <label>Riqueza</label>
+                <input id="riqueza" class="riqueza">
+            </fieldset>
+        </div>
+
+        <div id="datosFiltrado">
+            <fieldset><legend>Datos Filtrado</legend>
+                <label>Volumen inicial</label>
+                <input id="volumen_inicial" class="volumen_inicial">
+                <br><br><br>
+                <label>Volumen de agua</label>
+                <input id="volumen:agua" class="volumen_agua">                
+            </fieldset>
+        </div>
+        <div id="depositos">
+            <fieldset><legend>Depósitos</legend>                
+            </fieldset>
+        </div>
+    </div>
+        
+    <div id="notas">
+        <fieldset><legend>Notas</legend>
+            <textarea id="notasTexto"></textarea>
+        </fieldset>
+    </div>
+
+
+    <div id="botonera">
+        <button id="btnRetroceder" class="btnRetroceder">Retroceder</button>
+        <button id="btnFiltrar" class="btnFiltrar">Filtrar</button>
+    </div>
+
+    <div id="resultados" class="resultados">
+        <p id="displayFiltrado" name="displayFiltrado"></p>
+    </div>
+        
+  <!--  <div id="modal" class="mi-modal">
             <div class="mi-modal-contenido">
                 <p id="modalMsg"></p>
                 <button id="btnAceptar" class="mi-boton">Aceptar</button>
             </div>
-        </div>        
-    </div> 
-    
-    <!--<script src="/html/public/js/formpFiltado.js"></script>-->
+        </div>        -->
+    </div>     
+    <script src="/html/public/js/formFiltrado.js"></script>
 </body>
 </html>
