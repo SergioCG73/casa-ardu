1b404708417d551b862fa0225b1cb3c3e491b00d
Sergio Cano - Sótano <sergioc_g@hotmail.es>
2026-02-20 11:01:05 +0100

    Nuevo formulario

---
+++ app/views/formFiltrado.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/HTML/public/css/style.css" rel="stylesheet" type="text/css">
+    <title>FiltradoP18</title>    
+</head>
+<body>
+    <div id="contenedor" class="contenedor">
+        <h1 id="titulo-formulario">Filtrado Policloruro</h1>
+
+        <div id="formulario">
+            <div id="fabricaciones"><h1></h1>  </div>
+
+            <div id="reactores">
+                <fieldset>
+                    <legend>Reactores</legend>
+                    <div class="radio-group">
+                        <label class="radio-label"><input type="radio" name="sel_reactor" value="R1"> Reactor 1</label>
+                        <label class="radio-label"><input type="radio" name="sel_reactor" value="R2"> Reactor 2</label>
+                    </div>
+
+                    <div class ="input-group-Reactores">
+                        <label for="peso_inicial_reactor" class="input-label">Peso inicial reactor (Kg):</label>
+                        <input type="text" id="peso_inicial_reactor" name="peso_inicial_reactor"> 
+                    
+                        <label for="peso_final_reactor" class="input-label">Peso final reactor (Kg):</label>
+                        <input type="text" id="peso_final_reactor" name="peso_final_reactor"> 
+                    </div>
+                </fieldset>
+            </div>
+
+            <div id="recetas">
+                <fieldset>
+                    <legend>Recetas</legend>
+                    <div class="radio-group">
+                        <label class="radio-label"><input type="radio" name="sel_receta" value="P18"> P18</label>
+                        <label class="radio-label"><input type="radio" name="sel_receta" value="Papilla"> Papilla</label>
+                    </div>
+                </fieldset>
+            </div>
+
+            <div id="botonera" class="botonera">
+                <button id="btnRetroceder" class="boton">Retroceder</button>                
+                <button id="btnCrear" class="boton">Crear</button>                
+            </div>
+        </div>
+
+        <div id="resultados" class="resultados">
+            <p id="displayProduccion" name="displayProduccion"></p>
+        </div>
+        
+        <div id="modal" class="mi-modal">
+            <div class="mi-modal-contenido">
+                <p id="modalMsg"></p>
+                <button id="btnAceptar" class="mi-boton">Aceptar</button>
+            </div>
+        </div>        
+    </div> 
+    
+    <!--<script src="/html/public/js/formpFiltado.js"></script>-->
+</body>
+</html>
