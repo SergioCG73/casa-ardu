1b58e48090c25e6b288452e3dd4dbd9d82598995
Sergio Cano - Sótano <sergioc_g@hotmail.es>
2026-02-27 13:43:22 +0100

    Creacion de nuevos controlers

---
+++ app/controllers/CrearController.php
@@ -0,0 +1,8 @@
+<?php
+
+class CrearController {
+
+    public function fabricacion() {
+        require_once __DIR__ . "/../models/CrearP18.php";
+    }
+}
---
+++ app/controllers/TransferirController.php
@@ -0,0 +1,13 @@
+<?php
+
+class TransferirController {
+
+    public function mezclarAReactor() {
+        require_once __DIR__ . "/../models/transferirFabCurso.php";
+    }
+
+    public function reactorAM216() {
+        //require_once __DIR__ . "/../models/transferenciaFinal.php";
+        require_once __DIR__ . "/../models/transferirFabCurso.php";
+    }
+}
