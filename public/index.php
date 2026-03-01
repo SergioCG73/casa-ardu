65fd5f3b724d56544c9f827fa8060ab2acaad7dc
Sergio Cano - Sótano <sergioc_g@hotmail.es>
2026-03-01 10:43:33 +0100

    se modifica el GET de $a

--- public/index.php
+++ public/index.php
@@ -3,7 +3,8 @@
 //$c = $_GET['c'] ?? 'Home';  //Recoge el nombre del fichero y de la clase controller.
 $c = !empty($_GET['c']) ? $_GET['c'] : 'Home';
 //$a = $_GET['a'] ?? 'index'; //Recoge el método 
-$a = !empty($_GET['a']) ? $_GET['a'] : 'index';
+//$a = !empty($_GET['a']) ? $_GET['a'] : 'index';
+$a = !empty($_GET['a']) ? $_GET['a'] : 'home';
 
 
 $controllerFile = __DIR__ . '/../app/controllers/' . $c . 'Controller.php';
