5de01cf23a5870d062e6f8824f9a8ba239212051
Sergio Cano - Sótano <sergioc_g@hotmail.es>
2026-03-03 09:56:10 +0100

    Se agrega método fabricacionSulfato

--- app/controllers/CrearController.php
+++ app/controllers/CrearController.php
@@ -3,6 +3,10 @@
 class CrearController {
 
     public function fabricacion() {
-        require_once __DIR__ . "/../models/CrearP18.php";
+        require_once __DIR__ . "/../models/crearP18.php";
+    }
+
+    public function fabricacionSulfato() {
+        require_once __DIR__ . "/../models/crearSulfato.php";
     }
 }
