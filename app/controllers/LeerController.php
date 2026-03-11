0cda2d2b5efc9e2ca6039a33c526e467a947efa1
Sergio Cano - Sótano <sergioc_g@hotmail.es>
2026-03-11 07:48:12 +0100

    Se agrega método leerferrico()

--- app/controllers/LeerController.php
+++ app/controllers/LeerController.php
@@ -3,6 +3,9 @@
 class LeerController {
     public function lectura() {
         include __DIR__ . '/../models/lectura.php';
+    }
 
+    public function leerferrico() {
+        include __DIR__ . '/../models/leerFerrico.php';
     }
 }
