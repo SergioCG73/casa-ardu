4fec8540cb03a279a4746e0debd2e263a14980ff
Sergio Cano - Sótano <sergioc_g@hotmail.es>
2026-03-20 09:36:22 +0100

    Se cambia nombre del fichero a llamar de P18.php a viewP18.php

--- app/controllers/FormularioController.php
+++ app/controllers/FormularioController.php
@@ -3,7 +3,7 @@
 class FormularioController {    
     public function p18() {        
         //require_once __DIR__ . "/../views/formP18.php";
-        require_once __DIR__ . "/../views/P18.php";
+        require_once __DIR__ . "/../views/viewP18.php";
     }
 
     public function sulfato() {        
