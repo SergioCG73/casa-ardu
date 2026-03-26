19a86956b23d81ffd827aa6b062ae6b33689ef4a
Sergio Cano - Sótano <sergioc_g@hotmail.es>
2026-03-26 07:30:52 +0100

    Limpieza de código

--- app/models/borrarFabCurso.php
+++ app/models/borrarFabCurso.php
@@ -1,9 +1,5 @@
 <?php
 
-/*echo json_encode([
-    "file" => __FILE__
-]); exit;*/
-
 ob_clean();
 header('Content-Type: application/json; charset=utf-8');
 error_reporting(0);
@@ -12,7 +8,6 @@ ini_set('display_errors', 0);
 require_once("miconexion.php");
 
 $input = json_decode(file_get_contents("php://input"), true);
-
 $mezclador = $input["Mezclador"];
 $reactor = $input["Reactor"];
 
@@ -30,22 +25,10 @@ $stmt = $conexion->prepare($sqlUpdate);
 $stmt->bindParam(":mezclador", $mezclador);
 $rtdo = $stmt->execute();
 
-
 $sqlUpdate = "UPDATE equipos SET Estado = 'Vacio' WHERE Equipo_id = :reactor";
 $stmt = $conexion->prepare($sqlUpdate);
 $stmt->bindParam(":reactor", $reactor);
 $rtdo = $stmt->execute();
-
-/*echo json_encode([
-    "ok" => true,
-    "input" => $input,
-    "mezclador" => $mezclador,
-    "reactor" => $reactor,
-    "mensaje" => "Update OK"
-]); exit; */
-
-
-
 
 echo json_encode([
     "ok" => $ok,
