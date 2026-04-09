f7457a8dc42733472f57deecfe83b7fe1c015279
Sergio Cano - Sótano <sergioc_g@hotmail.es>
2026-04-09 18:25:22 +0200

    Se añade que si pulsas INTRO se valide el login

--- public/js/Login.js
+++ public/js/Login.js
@@ -1,8 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+
     const inputUsuario = document.getElementById("usuario");
     const inputPassword = document.getElementById("password");
     const btnIngresar = document.getElementById("btnIngresar");
+
+    inputPassword.addEventListener("keydown", (e) => {
+        if (e.key === "Enter") {
+            btnIngresar.click();
+        }
+    });
+
+    inputUsuario.addEventListener("keydown", (e) => {
+        if (e.key === "Enter") {
+            btnIngresar.click();
+        }
+    });
+
 
     btnIngresar.addEventListener("click", () => {
         const usuario = inputUsuario.value;
@@ -16,18 +29,18 @@ document.addEventListener("DOMContentLoaded", () => {
             method: "POST",
             body: datos
         })
-        .then(res => res.json())
-        .then(data => {
-            //console.log("Respuesta del servidor:", data); debugger
-            if (data.ok) {
-                    console.log("Correcto");                    
+            .then(res => res.json())
+            .then(data => {
+                //console.log("Respuesta del servidor:", data); debugger
+                if (data.ok) {
+                    console.log("Correcto");
                     //localStorage.setItem("rol", data.rol);
                     window.location.href = "index.php?c=Formulario&a=home";
-            } else {
+                } else {
                     alert("Usuario o contraseña incorrectos");
-            }
-        })
-        .catch(err => console.error("Error:", err));
+                }
+            })
+            .catch(err => console.error("Error:", err));
     });
 
 });
