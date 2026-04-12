11ca41bd07694f8abf3ddcd1573eb1897c09edf2
Sergio Cano - Sótano <sergioc_g@hotmail.es>
2026-04-12 09:34:38 +0200

    Se agrega formatearMiles()

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,23 +1,27 @@
 function mostrarModal(mensaje) {
-        const modal = document.getElementById("modal");
-        document.getElementById("modalMsg").textContent = mensaje;
-        modal.hidden = false;
-        modal.style.display = "flex";
-    }
+    const modal = document.getElementById("modal");
+    document.getElementById("modalMsg").textContent = mensaje;
+    modal.hidden = false;
+    modal.style.display = "flex";
+}
 
-    function cerrarModal() {
-        const modal = document.getElementById("modal");
-        modal.hidden = true;
-        modal.style.display = "none";        
-        window.location.href = "index.php?c=Formulario&a=home";
-    }
+function cerrarModal() {
+    const modal = document.getElementById("modal");
+    modal.hidden = true;
+    modal.style.display = "none";
+    window.location.href = "index.php?c=Formulario&a=home";
+}
 
-    function formatearNumero(input) {
-        let valor = input.value;
-        if (valor == null) return;
-        valor = String(valor).replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-        input.value = valor;
-        return valor;
-    }
+function formatearNumero(input) {
+    let valor = input.value;
+    if (valor == null) return;
+    valor = String(valor).replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    input.value = valor;
+    return valor;
+}
 
-   
+function formatearMiles(num) {
+    return num
+        .toString()
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
