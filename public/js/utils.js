7b8015953740d01e2637ef9b7210750c4b88164e
Sergio Cano - Sótano <sergioc_g@hotmail.es>
2026-04-12 09:26:23 +0200

    Funciones genéricas a los JS

---
+++ public/js/utils.js
@@ -0,0 +1,23 @@
+function mostrarModal(mensaje) {
+        const modal = document.getElementById("modal");
+        document.getElementById("modalMsg").textContent = mensaje;
+        modal.hidden = false;
+        modal.style.display = "flex";
+    }
+
+    function cerrarModal() {
+        const modal = document.getElementById("modal");
+        modal.hidden = true;
+        modal.style.display = "none";        
+        window.location.href = "index.php?c=Formulario&a=home";
+    }
+
+    function formatearNumero(input) {
+        let valor = input.value;
+        if (valor == null) return;
+        valor = String(valor).replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+        input.value = valor;
+        return valor;
+    }
+
+   
