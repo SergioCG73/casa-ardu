381051ac028a43921f149ad34e04d7d1b8cf7318
Sergio Cano - Sótano <sergioc_g@hotmail.es>
2026-04-20 09:49:38 +0200

    Nuevo HTML

--- app/views/viewLaboratorio.php
+++ app/views/viewLaboratorio.php
@@ -1,17 +1,31 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/styleLaboratorio.css" rel="stylesheet" type="text/css">
     <title>Laboratorio</title>
 </head>
+
 <body>
     <h1>Laboratorio</h1>
     <div id="filtros" class="filtros">
-        <div id="productos" class="productos"></div>
+        <fieldset>
+            <legend>Filtro</legend>
+            <div id="productos" class="productos"></div>
+            <div id="fechas" class="fechas">
+                <label>Desde </label>
+                <input type="date" id = "desde">
+                <label>Hasta</label>
+                <input type="date" id = "hasta">
+            </div>
+        </fieldset>
     </div>
 
-    <div id="tabla" class="tabla"></div>    
+    <div id="tabla" class="tabla"></div>
 </body>
-    <script src = "js/utils.js"></script>
+<script src="js/utils.js"></script>
+<script src="js/Laboratorio.js"></script>
+
 </html>
