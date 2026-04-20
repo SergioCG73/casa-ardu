c33dad5070d7ec67e3a0a4bae3cda0f0bc7a339b
Sergio Cano - Sótano <sergioc_g@hotmail.es>
2026-04-20 09:49:09 +0200

    Nuevo método para leer productos de la tabla recetas

--- app/models/LeerProductos.php
+++ app/models/LeerProductos.php
@@ -1,5 +1,22 @@
 <?php
+/*echo json_encode([
+    "file" => __FILE__
+]); exit;*/
 
+ob_clean();
+header('Content-Type: application/json; charset=utf-8');
+error_reporting(0);
+ini_set('display_errors', 0);
 
+require_once("miconexion.php");
+
+$sqlSelect = "SELECT DISTINCT ProductoFabricado FROM recetas";
+$stmt = $conexion->prepare($sqlSelect);
+$stmt->execute();
+$productos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo json_encode([
+    "productos" => $productos
+]); exit;
 
 ?>
