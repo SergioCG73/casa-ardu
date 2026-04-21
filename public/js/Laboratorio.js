68728d2c92d73f9f38d200ceb3da8c29d895c69a
Sergio Cano - Sótano <sergioc_g@hotmail.es>
2026-04-21 09:39:26 +0200

    Implementación de búsqueda por producto

--- public/js/Laboratorio.js
+++ public/js/Laboratorio.js
@@ -1,28 +1,32 @@
 document.addEventListener("DOMContentLoaded", async () => {
-    const divFormulario = document.getElementById("filtros");    
-    const DateDesde = document.getElementById("desde");    
+    const divFormulario = document.getElementById("filtros");
+    const btnBuscar = document.getElementById("btnBuscar");
+    const DateDesde = document.getElementById("desde");
     const DateHasta = document.getElementById("hasta");
 
     DateDesde.addEventListener("change", () => {
-        console.log(DateDesde.value);        
+        console.log(DateDesde.value);
     })
 
-    DateHasta.addEventListener("change", () => {        
+    DateHasta.addEventListener("change", () => {
         console.log(DateHasta.value);
     })
 
     if (DateHasta.value < DateDesde.value) {
         alert("debe elegir una fecha Hasta mayor que Desde");
     }
-    
 
-    // Obtener produtos fabricados
+    // Obtener productos fabricados    
     const data = await cargarProductos();
     const productos = data.productos;
+
     // Generar checkboxes
     generarCheckBoxesProductos(productos);
 
-    const dateDesde = document.getElementById("desde");
+    btnBuscar.addEventListener("click", () => {
+        const productosSeleccionados = [...document.querySelectorAll("input[name='producto']:checked")].map(cb => cb.value);        
+        cargarAnaliticas(productosSeleccionados);
+    })
 
-
+    
 });
