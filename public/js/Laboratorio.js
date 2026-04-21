36139c65e7354dccb5f6d9ee7b7bab659fbe2824
Sergio Cano - Sótano <sergioc_g@hotmail.es>
2026-04-21 13:07:19 +0200

    Se agrega llamada a CargarConfig(), configurarSelectCantida(), cargarAnaliticas()

--- public/js/Laboratorio.js
+++ public/js/Laboratorio.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", async () => {
     const divFormulario = document.getElementById("filtros");
+    const selectMostrar = document.getElementById("cantidad");
     const btnBuscar = document.getElementById("btnBuscar");
     const DateDesde = document.getElementById("desde");
     const DateHasta = document.getElementById("hasta");
@@ -19,14 +20,23 @@ document.addEventListener("DOMContentLoaded", async () => {
     // Obtener productos fabricados    
     const data = await cargarProductos();
     const productos = data.productos;
-
     // Generar checkboxes
     generarCheckBoxesProductos(productos);
 
+    // Obtener archivo config.json
+    const config = await CargarConfig();    
+    const min = config.limitesSelect.inferior;
+    const max = config.limitesSelect.superior;
+
+    // Generar select
+    configurarSelectCantidad(config);    
+
     btnBuscar.addEventListener("click", () => {
-        const productosSeleccionados = [...document.querySelectorAll("input[name='producto']:checked")].map(cb => cb.value);        
-        cargarAnaliticas(productosSeleccionados);
-    })
+        const productosSeleccionados = [...document.querySelectorAll("input[name='producto']:checked")].map(cb => cb.value);
+        const limite = selectMostrar.value;
+        
+        cargarAnaliticas(productosSeleccionados, limite);
+    });
 
     
 });
